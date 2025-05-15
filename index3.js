4e5d4754e52f24fbceedfb0e69c031454d417139
Ganesh Pachbhai <ganeshpachbhai04@gmail.com>
2025-05-15 17:08:51 +0530

    commit update

--- index3.js
+++ index3.js
@@ -2,13 +2,13 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-
-function logger(req, res, next) {
-  console.log("Request Method: ", req.method);
-  console.log("Request URL: ", req.url);
+// app.use(custom_function);
+function logger(req, res, next){
+  console.log("Request Method:",req.method);
+  console.log("Request URL:",req.url);
   next();
 }
-app.use(logger);
+ app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
--- index4.js
+++ index4.js
@@ -1,32 +1,33 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import bodyParser  from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-var bandName = "";
-
+let bandname="";
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 function bandNameGenerator(req, res, next) {
   console.log(req.body);
-  bandName = req.body["street"] + req.body["pet"];
+  bandname = req.body["street"] +" "+req.body["pet"];
   next();
 }
 
 app.use(bandNameGenerator);
 
-app.get("/", (req, res) => {
+app.get("/",(req, res)=>{
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/submit", (req, res) => {
-  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+app.post("/submit",(req, res)=>{
+  // console.log(req.body);
+  res.send(`<h1>Your band name is </h1><h2> ${bandname}</h2>`);
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listning on port ${port}`);
 });
